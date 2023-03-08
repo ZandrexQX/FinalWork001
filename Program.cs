07b91f76894eb1e059556ade1181d3a53562606f
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-03-08 19:36:31 +0300

    edit 001

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Final Task");
+Console.WriteLine("------");
+
+string[] Arr3Chars(string[] arr)
+{
+    int count = 0;
+    Console.Write("");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 4) count++;
+    }
+    string[] arr2 = new string[count];
+    count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 4){
+            arr2[count] = arr[i];
+            count++;
+        }
+    }
+    return arr2;
+}
+
+
+
+Console.Write("Введите строки через пробел: ");
+string[] strArray = Console.ReadLine().Split("");
+Console.ReadLine();
+
+/*--------------------------------------------*/
